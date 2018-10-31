50721c00c55391a1f16d65899b8ce4f053002c29
LouisBertin <bertin.louis7@gmail.com>
2018-10-31 18:37:01 -0400

    build signin logic

--- server/routes/api/signin.js
+++ server/routes/api/signin.js
@@ -1,3 +1,55 @@
-module.exports = (app) => {
+import User from '../../models/User'
 
+module.exports = (app) => {
+    app.post('/api/account/signup', (req, res, next) => {
+        const { body } = req
+        const {
+            firstName,
+            lastName,
+            email,
+            password
+        } = body
+
+        if (!firstName) {
+            res.end({
+                success: false,
+                message: 'Error: First name cannot be blank.'
+            })
+        }
+
+        User.find({
+            email: email
+        }, (err, previousUser) => {
+            if (err) {
+                res.end({
+                    success: false,
+                    message: 'Error: Server Error.'
+                })
+            } else if (previousUser > 0) {
+                res.end({
+                    success: false,
+                    message: 'Error: Account already exist.'
+                })
+            } else {
+                const newUser = new User();
+
+                newUser.firstName = firstName;
+                newUser.lastName = lastName;
+                newUser.email = email;
+                newUser.password = newUser.generateHash(password);
+                newUser.save((err, user) => {
+                    if (err) {
+                        res.end({
+                            success: false,
+                            message: 'Error: Server error'
+                        })
+                    }
+                    res.end({
+                        success: true,
+                        message: 'Sign up'
+                    })
+                })
+            }
+        })
+    })
 }
