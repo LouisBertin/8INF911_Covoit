48386e4790649560087c90f4b52c1e0920e05cad
LouisBertin <bertin.louis7@gmail.com>
2018-11-18 12:47:12 -0500

    circle fill color configuration

--- client/src/App/utils/config.js
+++ client/src/App/utils/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     MAPBOX_API_TOKEN: 'pk.eyJ1IjoibG91aXNiZXJ0aW4iLCJhIjoiY2pvOHo5OWM2MDAwazNycDBqdmg4dDNlaSJ9.dG2toPupuK_AbxZtAyqINQ',
     SITE_URL: window.location.origin,
-    CIRCLE_SIZE: 300
+    CIRCLE_SIZE: 300,
+    CIRCLE_COLOR: '#29AB87'
 }
