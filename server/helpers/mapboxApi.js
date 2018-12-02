67b85d37835081360a67c16e3079d3e6c52b999a
LouisBertin <bertin.louis7@gmail.com>
2018-12-02 10:40:01 -0500

    wrap axios call with try catch

--- server/helpers/mapboxApi.js
+++ server/helpers/mapboxApi.js
@@ -4,12 +4,20 @@ const axios = require('axios');
 module.exports = {
     // get place data with coordinates
     placeData: async function (lng, lat) {
-        const response = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=` + config.mapboxToken)
-        return response.data.features[0];
+        try {
+            const response = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=` + config.mapboxToken)
+            return response.data.features[0];
+        } catch (e) {
+            console.log(e.response.data)
+        }
     },
     // get place data with coordinates
     directionDistance: async function ([latStart, lngStart], [latEnd, lngEnd]) {
-        const response = await axios(`https://api.mapbox.com/directions/v5/mapbox/walking/${lngStart},${latStart};${lngEnd},${latEnd}?access_token=` + config.mapboxToken)
-        return response.data.routes[0].distance;
+        try {
+            const response = await axios(`https://api.mapbox.com/directions/v5/mapbox/walking/${lngStart},${latStart};${lngEnd},${latEnd}?access_token=` + config.mapboxToken)
+            return response.data.routes[0].distance;
+        } catch (e) {
+            console.log(e.response.data)
+        }
     }
 }
